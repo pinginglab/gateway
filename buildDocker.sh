71f109fdae70ba4f2a37c1046ec2086fd1fd235d
yerikyu <treestore@example.com>
2020-02-28 23:01:33 +0800

    更新打包脚本

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./gradlew bootJar -Pprod jibDockerBuild
 #docker tag gateway:latest registry.cn-hangzhou.aliyuncs.com/pingsec/compose/gateway:latest
-docker tag gateway:latest ccr.ccs.tencentyun.com/pingsec/compose/gateway:1.0
-docker push ccr.ccs.tencentyun.com/pingsec/compose/gateway:1.0
+docker tag gateway:latest ccr.ccs.tencentyun.com/compose/gateway:1.0
+docker push ccr.ccs.tencentyun.com/compose/gateway:1.0
